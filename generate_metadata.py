d56476443730393335d5b9a9d6545acded0006e0
ksx <38215360+anonymousKo@users.noreply.github.com>
2023-06-05 15:38:27 +0800

    Create generate_metadata.py

---
+++ generate_metadata.py
@@ -0,0 +1,43 @@
+import os
+import frontmatter
+
+metadata_template = """---
+title: {}
+date: {}
+---
+
+{}"""
+
+def has_metadata(file_path):
+    with open(file_path, "r") as file:
+        content = file.read()
+
+    try:
+        frontmatter.load(content)
+        return True
+    except frontmatter.exceptions.InvalidFrontmatterError:
+        return False
+
+def generate_metadata(file_path):
+    if has_metadata(file_path):
+        print(f"Skipping {file_path} (metadata already exists)")
+        return
+
+    file_name = os.path.basename(file_path)
+    date, title = file_name.split("-", 2)[0:2]
+
+    with open(file_path, "r") as file:
+        content = file.read()
+
+    metadata_content = metadata_template.format(title, date, content)
+
+    with open(file_path, "w") as file:
+        file.write(metadata_content)
+        print(f"Generated metadata for {file_path}")
+
+# Iterate through all Markdown files in the repository and generate metadata
+for root, dirs, files in os.walk("."):
+    for file in files:
+        if file.endswith(".md"):
+            file_path = os.path.join(root, file)
+            generate_metadata(file_path)
